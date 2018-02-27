09ea868d6f449642f1d821c5e243464035bb3d9f
Vysakh Sreenivasan <diplomatv@gmail.com>
2018-02-27 10:35:33 +0530

    Strip extra pages

--- src/pages/react-intro/javascript-essentials.jsx
+++ src/pages/react-intro/javascript-essentials.jsx
@@ -8,7 +8,10 @@ const api = `
 
 export default (props) =>
     <MainLayout>
-        <Block title="1. Variable declarations - 3 ways" value={api} />
-        <p>Let's learn few more features that I think is a must if you work on any js project</p>
+        <Block title="1. String related functions" value={api} />
+        <Block title="2. Array CRUD" value={api} />
+        <Block title="3. Object CRUD" value={api} />
+        <Block title="4. Loops with array" value={api} />
+        <Block title="5. class, constructor, object variable" value={api} />
         <CtaButton to="/react-intro/javascript-crash-course">I'm ready, lets go</CtaButton>
     </MainLayout>
