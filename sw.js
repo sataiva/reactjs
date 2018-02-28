fc163d0045d03072998a6e937f3fe7e301508c12
Vysakh Sreenivasan <diplomatv@gmail.com>
2018-02-28 17:37:36 +0530

    Updates

--- sw.js
+++ sw.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/app-4b56f9e0442d2f774a34.js","d640a624cd66bc3403f57e96d7e29fb8"],["/index.html","83dc7872f6163e6ab190f09f58d76b6a"],["/manifest.json","e82426d233204e68debc5c31d566692a"],["/offline-plugin-app-shell-fallback/index.html","3dc83b9a27b277478d4a298b97dd8830"]];
+var precacheConfig = [["/app-4b56f9e0442d2f774a34.js","d640a624cd66bc3403f57e96d7e29fb8"],["/index.html","f274de3720451a43afb90190f27146b4"],["/manifest.json","e82426d233204e68debc5c31d566692a"],["/offline-plugin-app-shell-fallback/index.html","bcd601e8e01c48994d50383f3198eb3a"]];
 var cacheName = 'sw-precache-v3-gatsby-plugin-offline-' + (self.registration ? self.registration.scope : '');
 
 
